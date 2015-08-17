b9eca2dab04f558189be168585cf2ed09efd2ca0
Raghav <raghav.s@reglobe.in>
2015-08-17 12:00:49 +0530

    First Commit

--- app/src/main/java/in/reglobe/testgit/MainActivity.java
+++ app/src/main/java/in/reglobe/testgit/MainActivity.java
@@ -1,7 +1,7 @@
 package in.reglobe.testgit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
